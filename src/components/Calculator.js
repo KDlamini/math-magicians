ab9465370b012115fc1b2c7f97d9fa9f1ee1f37b
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-10-21 22:26:16 +0200

    refactor class Calculator component with hooks.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Screen from './Screen';
 import Button from './Button';
 import calculate from '../logic/calculate';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
+const Calculator = () => {
+  const [total, setTotal] = useState(0);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+  const handleClick = (value) => {
+    const results = calculate({ total, next, operation }, value);
+    setTotal(results.total);
+    setNext(results.next);
+    setOperation(results.operation);
+  };
 
-  handleClick = (value) => {
-    this.setState((prevState) => calculate(prevState, value));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-
-    return (
-      <div id="calculator">
-        <Screen id="display" total={total} next={next} operation={operation} />
-        <Button id="clear" name="AC" handleClick={this.handleClick} />
-        <Button id="delete" name="+/-" handleClick={this.handleClick} />
-        <Button id="delete" name="%" handleClick={this.handleClick} />
-        <Button id="divide" name="÷" handleClick={this.handleClick} />
-        <Button id="seven" name="7" handleClick={this.handleClick} />
-        <Button id="eight" name="8" handleClick={this.handleClick} />
-        <Button id="nine" name="9" handleClick={this.handleClick} />
-        <Button id="multiply" name="x" handleClick={this.handleClick} />
-        <Button id="four" name="4" handleClick={this.handleClick} />
-        <Button id="five" name="5" handleClick={this.handleClick} />
-        <Button id="six" name="6" handleClick={this.handleClick} />
-        <Button id="subtract" name="-" handleClick={this.handleClick} />
-        <Button id="one" name="1" handleClick={this.handleClick} />
-        <Button id="two" name="2" handleClick={this.handleClick} />
-        <Button id="three" name="3" handleClick={this.handleClick} />
-        <Button id="add" name="+" handleClick={this.handleClick} />
-        <Button id="zero" name="0" handleClick={this.handleClick} />
-        <Button id="decimal" name="." handleClick={this.handleClick} />
-        <Button id="equals" name="=" handleClick={this.handleClick} />
-      </div>
-    );
-  }
-}
+  return (
+    <div id="calculator">
+      <Screen id="display" total={total} next={next} operation={operation} />
+      <Button id="clear" name="AC" handleClick={handleClick} />
+      <Button id="delete" name="+/-" handleClick={handleClick} />
+      <Button id="delete" name="%" handleClick={handleClick} />
+      <Button id="divide" name="÷" handleClick={handleClick} />
+      <Button id="seven" name="7" handleClick={handleClick} />
+      <Button id="eight" name="8" handleClick={handleClick} />
+      <Button id="nine" name="9" handleClick={handleClick} />
+      <Button id="multiply" name="x" handleClick={handleClick} />
+      <Button id="four" name="4" handleClick={handleClick} />
+      <Button id="five" name="5" handleClick={handleClick} />
+      <Button id="six" name="6" handleClick={handleClick} />
+      <Button id="subtract" name="-" handleClick={handleClick} />
+      <Button id="one" name="1" handleClick={handleClick} />
+      <Button id="two" name="2" handleClick={handleClick} />
+      <Button id="three" name="3" handleClick={handleClick} />
+      <Button id="add" name="+" handleClick={handleClick} />
+      <Button id="zero" name="0" handleClick={handleClick} />
+      <Button id="decimal" name="." handleClick={handleClick} />
+      <Button id="equals" name="=" handleClick={handleClick} />
+    </div>
+  );
+};
 
 export default Calculator;
