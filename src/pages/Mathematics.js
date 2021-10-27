4c0aa1a9ee2cd41429a5a531ba12cd63afa12f9e
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-10-27 16:01:30 +0200

    Add styling for Calculator page.

--- src/pages/Mathematics.js
+++ src/pages/Mathematics.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Calculator from '../components/Calculator';
+import '../styles/Mathematics.css';
 
 function Mathematics() {
   return (
     <section className="calculator-page">
+      <div className="calculator-content">
+        <p>Let&apos;s do some math!</p>
+      </div>
       <Calculator />
     </section>
   );
