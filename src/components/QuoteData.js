6651b215713d71a830c7a459bdd9d01337df26ad
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-10-27 18:07:56 +0200

    Randomly generate quote and render on page.

---
+++ src/components/QuoteData.js
@@ -0,0 +1,56 @@
+const QuoteData = () => {
+  const quotes = [
+    {
+      quote: 'In real life, I assure you, there is no such thing as algebra.',
+      author: 'Fran Lebowitz',
+    },
+    {
+      quote: 'Mathematics expresses values that reflect the cosmos, including orderliness, balance, harmony, logic, and abstract beauty.',
+      author: 'Deepak Chopra',
+    },
+    {
+      quote: 'The difference between the poet and the mathematician is that the poet tries to get his head into the heavens while the mathematician tries to get the heavens into his head.',
+      author: 'G.K. Chesterton',
+    },
+    {
+      quote: 'But in my opinion, all things in nature occur mathematically.',
+      author: 'Rene Decartes',
+    },
+    {
+      quote: 'Prime numbers are what is left when you have taken all the patterns away. I think prime numbers are like life. They are very logical but you could never work out the rules, even if you spent all your time thinking about them',
+      author: 'Mark Haddon, The Curious Incident of the Dog in the Night-Time',
+    },
+    {
+      quote: 'Pure mathematics is, in its way, the poetry of logical ideas.',
+      author: 'Albert Einstein, German theoretical physicist',
+    },
+    {
+      quote: 'Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers.',
+      author: 'Shakuntala Devi, Indian writer and mental calculator',
+    },
+    {
+      quote: 'Mathematics is the most beautiful and most powerful creation of the human spirit.',
+      author: 'Stefan Banach, Polish mathematician',
+    },
+    {
+      quote: 'Mathematics as an expression of the human mind reflects the active will, the contemplative reason, and the desire for aesthetic perfection. Its basic elements are logic and intuition, analysis and construction, generality and individuality.',
+      author: 'Richard Courant, German-American mathematician',
+    },
+    {
+      quote: 'As far as the laws of mathematics refer to reality, they are not certain, and as far as they are certain, they do not refer to reality.',
+      author: 'Albert Einstein',
+    },
+    {
+      quote: 'Mathematics knows no races or geographic boundaries; for mathematics, the cultural world is one country.',
+      author: 'David Hilbert, German mathematician',
+    },
+    {
+      quote: 'Mathematics are the result of mysterious powers which no one understands, and which the unconscious recognition of beauty must play an important part. Out of an infinity of designs a mathematician chooses one pattern for beauty’s sake and pulls it down to earth.',
+      author: 'Marston Morse, American mathematician',
+    },
+  ];
+
+  return quotes;
+};
+
+export default QuoteData;
--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import QuoteData from '../components/QuoteData';
 import '../styles/Quotes.css';
 
 function Quotes() {
+  const quotes = QuoteData();
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const RandomQuote = quotes[randomIndex];
+
+    setQuote(RandomQuote.quote);
+    setAuthor(RandomQuote.author);
+  });
+
   return (
-    <section className="quote-page">Quotes Page</section>
+    <section className="quote-page">
+      <p>{quote}</p>
+      <h3>{author}</h3>
+    </section>
   );
 }
 
